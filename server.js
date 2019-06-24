933704686bc75c558a10bf10af391076a948f172
Pritish Mishra <c-pritish.mishra@timesgroup.com>
2019-06-24 14:13:01 +0530

    again

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require('express')
+var express = require('express');
+const path = require("path");
 var app = express();
 
 app.get('/', (req,res) => {
-    res.send("hello world");
+    res.sendFile(path.join(__dirname,'index.html'));
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("App running");
 })
